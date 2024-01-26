5516114e8242d7ed7adacd9f0479e9a9d50bb41e
kamron9 <kamronalimov80@gmail.com>
2024-01-26 19:40:37 +0500

    carousel text changed

--- data/carouselData.js
+++ data/carouselData.js
@@ -1,26 +1,26 @@
 export const carouselData = [
-	{
-		id: 1,
-		title: 'oddiy Bogibek brat title',
-		description:
-			'Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris ultricies rutrum urna',
-		category: 'oddiy',
-		imgLink: '/carousel-1.jpeg',
-	},
-	{
-		id: 2,
-		title: 'oddiy Bogibek brat title-1',
-		description:
-			'Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris ultricies rutrum urna, id volutpat justo fringilla nec. Maecenas vestibulum massa eget tincidunt tempus. Sed ',
-		category: 'oddiy',
-		imgLink: '/carousel-2.avif',
-	},
-	{
-		id: 3,
-		title: 'oddiy Bogibek brat title-2',
-		description:
-			'Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris ultricies rutrum urna, id volutpat justo fringilla nec. Maecenas vestibulum massa eget tincidunt tempus. Sed ',
-		category: 'oddiy',
-		imgLink: '/carousel-1.jpeg',
-	},
-]
+  {
+    id: 1,
+    title: "carousel title-1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris ultricies rutrum urna",
+    category: "oddiy",
+    imgLink: "/carousel-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "carousel  title-2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris ultricies rutrum urna, id volutpat justo fringilla nec. Maecenas vestibulum massa eget tincidunt tempus. Sed ",
+    category: "oddiy",
+    imgLink: "/carousel-2.avif",
+  },
+  {
+    id: 3,
+    title: "carousel title-3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Mauris ultricies rutrum urna, id volutpat justo fringilla nec. Maecenas vestibulum massa eget tincidunt tempus. Sed ",
+    category: "oddiy",
+    imgLink: "/carousel-1.jpeg",
+  },
+];
