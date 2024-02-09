d2c9b41ab0af68fa581fdeb7507b384a89d8af8e
kamron9 <kamronalimov80@gmail.com>
2024-02-09 21:34:18 +0500

    carousel bug fixed

--- components/Carousel/CarouselSlides.jsx
+++ components/Carousel/CarouselSlides.jsx
@@ -1,26 +1,25 @@
-import Image from "next/image";
-import { memo } from "react";
+import Image from 'next/image'
 
-export const CarouselNextBtn = memo(({ nextSlide }) => (
-  <div
-    onClick={nextSlide}
-    className={"carousel-btn"}
-    style={{ marginRight: "10px", cursor: "pointer" }}
-  >
-    <Image
-      src={"/right-arrow.svg"}
-      alt={"right-arrow"}
-      width={30}
-      height={30}
-    />
-  </div>
-));
-export const carouselPreviousBtn = memo(({ previousSlide }) => (
-  <div
-    onClick={previousSlide}
-    className={"carousel-btn"}
-    style={{ marginLeft: "10px", cursor: "pointer" }}
-  >
-    <Image src={"/left-arrow.svg"} alt={"left-arrow"} width={30} height={30} />
-  </div>
-));
+export const CarouselNextBtn = ({ nextSlide }) => (
+	<div
+		onClick={nextSlide}
+		className={'carousel-btn'}
+		style={{ marginRight: '10px', cursor: 'pointer' }}
+	>
+		<Image
+			src={'/right-arrow.svg'}
+			alt={'right-arrow'}
+			width={30}
+			height={30}
+		/>
+	</div>
+)
+export const carouselPreviousBtn = ({ previousSlide }) => (
+	<div
+		onClick={previousSlide}
+		className={'carousel-btn'}
+		style={{ marginLeft: '10px', cursor: 'pointer' }}
+	>
+		<Image src={'/left-arrow.svg'} alt={'left-arrow'} width={30} height={30} />
+	</div>
+)
