ae497a98debbde131744fafd589ad06164a5755a
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-03-09 20:56:13 +0500

    footer redesigned

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,117 +1,94 @@
-"use client";
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
-import Image from "next/image";
-import Link from "next/link";
-const Footer = () => {
-  return (
-    <Box as={"footer"} id={"footer"} bg={"rgb(244 245 247)"} padding={"40px 0"}>
-      <Container maxW={"1400px"}>
-        <Box
-          display={"flex"}
-          alignItems={{ base: "start" }}
-          justifyContent={{ base: "start", sm: "space-between" }}
-          flexWrap={"wrap"}
-          gap={"30px"}
-        >
-          {/* logo */}
-          <Box display={"flex"} flexDirection={"column"} gap={"10px"}>
-            <Heading>Logo</Heading>
-            {/* contacts */}
-            <Box>
-              <Text>Телефон поддержки</Text>
-              <a href={"tel:919192020"}>
-                <Text as={"b"}>+998 (91) 919 20 20</Text>
-              </a>
-            </Box>
-            {/*  address */}
-            <Box display={"flex"} alignItems={"center"} gap={"3px"}>
-              <Image
-                src={"/location.svg"}
-                alt={"location"}
-                width={20}
-                height={20}
-              />
-              <Text>urgench darital</Text>
-            </Box>
-            {/*  email  */}
-            <Box display={"flex"} alignItems={"center"} gap={"3px"}>
-              <Image
-                src={"/email.svg"}
-                alt={"location"}
-                width={20}
-                height={20}
-              />
-              <a href="mailto:kamronalimov80@gmail.com">
-                <Text>kamronalimov80@gmail.com</Text>
-              </a>
-            </Box>
-          </Box>
-          {/* navigations */}
-          <Box
-            as={"nav"}
-            display={{ base: "none", lg: "flex" }}
-            flexDirection={"column"}
-            gap={"10px"}
-          >
-            <Link href={"/"}>
-              <Text as={"span"} color={"purple"}>
-                buyurtma berish
-              </Text>
-            </Link>
-            <Link href={"/"}>home</Link>
-            <Link href={"/"}>about</Link>
-            <Link href={"/"}>contact</Link>
-          </Box>
-          {/*  form  */}
-        </Box>
-        <hr style={{ margin: "20px 0" }} />
-        <Box
-          display={"flex"}
-          alignItems={{ base: "start", sm: "center" }}
-          flexDirection={{ base: "column", sm: "row" }}
-          gap={"20px"}
-          justifyContent={"space-between"}
-        >
-          <Text as={"span"} fontSize={"14px"}>
-            © Sofiya 2023 - 2024 All rights reserved
-          </Text>
-          {/*  socials */}
-          <Box display={"flex"} alignItems={"center"} gap={"5px"}>
-            {/* telegram icon */}
-            <a href={"https://telegram.com"}>
-              <Image
-                src={"/telegram.svg"}
-                alt={"telegram icon"}
-                width={20}
-                height={20}
-                style={{ width: "25px", height: "25px" }}
-              />
-            </a>
-            {/* instagram icon */}
-            <a href={"https://facebook.com"}>
-              <Image
-                src={"/instagram.svg"}
-                alt={"telegram icon"}
-                width={20}
-                height={20}
-                style={{ width: "25px", height: "25px" }}
-              />
-            </a>
-            {/* facebook icon */}
-            <a href={"https://instagram.com"}>
-              <Image
-                src={"/facebook.svg"}
-                alt={"telegram icon"}
-                width={20}
-                height={20}
-                style={{ width: "25px", height: "25px" }}
-              />
-            </a>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
-  );
-};
+import emailIcon from '@/public/assets/icons/email.svg'
+import facebookIcon from '@/public/assets/icons/facebook.svg'
+import instagramIcon from '@/public/assets/icons/instagram.svg'
+import mapIcon from '@/public/assets/icons/map.svg'
+import phoneIcon from '@/public/assets/icons/phone.svg'
+import telegramIcon from '@/public/assets/icons/telegram.svg'
+import {
+	Box,
+	Container,
+	Flex,
+	Grid,
+	GridItem,
+	Heading,
+	Text,
+} from '@chakra-ui/react'
+import Image from 'next/image'
 
-export default Footer;
+const Footer = () => {
+	return (
+		<Box as={'footer'} id={'footer'} bg={'rgb(244 245 247)'} padding={'40px 0'}>
+			<Container maxW={'1400px'}>
+				<Grid
+					gap={'30px'}
+					px={'20px'}
+					templateColumns={{
+						base: 'repeat(1, 1fr)',
+						md: 'repeat(3, 1fr)',
+						lg: 'repeat(4, 1fr)',
+					}}
+				>
+					{/* logo */}
+					<GridItem>
+						<Heading>Logo</Heading>
+					</GridItem>
+					{/* social media */}
+					<GridItem>
+						<Text>Ijtimoiy tarmoqlarimiz</Text>
+						<Flex mt={'10px'} alignItems={'center'} gap={'10px'}>
+							<a href={'#'}>
+								<Image src={telegramIcon} alt={'telegram'} height={25} />
+							</a>
+							<a href={'#'}>
+								<Image src={instagramIcon} alt={'instagram'} height={32} />
+							</a>
+							<a href={'#'}>
+								<Image src={facebookIcon} alt={'facebook'} height={25} />
+							</a>
+						</Flex>
+					</GridItem>
+
+					{/* contacts */}
+					<GridItem>
+						{/* phone number */}
+						<Flex alignItems={'center'} gap={'10px'} mb={'10px'}>
+							<Image src={phoneIcon} height={25} alt='phone icon' />
+							<a href={'tel:919192020'}>
+								<Text>+998 (91) 919 20 20</Text>
+							</a>
+						</Flex>
+						{/* email */}
+						<Flex alignItems={'center'} gap={'10px'}>
+							<Image src={emailIcon} alt={'email'} height={25} />
+							<a href='mailto:kamronalimov80@gmail.com'>
+								<Text>kamronalimov80@gmail.com</Text>
+							</a>
+						</Flex>
+					</GridItem>
+					{/*  address */}
+
+					<GridItem px={{ base: '0', lg: '10px' }}>
+						<Text mb={'10px'}>
+							Toshkent shahri, Сhilanzor Yakkabog’ ko’chasi 25-uy
+						</Text>
+						<a href='#'>
+							<Box
+								display={'inline-flex'}
+								alignItems={'center'}
+								gap={'10px'}
+								bg={'white'}
+								padding={'10px 20px'}
+								borderRadius={'12px'}
+							>
+								<Image src={mapIcon} alt={'location'} height={20} />
+								<Text fontSize={'16px'}>Xarita</Text>
+							</Box>
+						</a>
+					</GridItem>
+				</Grid>
+			</Container>
+		</Box>
+	)
+}
+
+export default Footer
