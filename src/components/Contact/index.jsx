af3fb780171158f4f938ac9d9e3987a63368ad90
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-04-18 04:06:10 +0500

    handling server error

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,49 +1,49 @@
-import ContactForm from "@/components/Form";
-import { Box, Image as ChakraImage, Flex, Heading } from "@chakra-ui/react";
-import { useTranslations } from "next-intl";
-import Image from "next/image";
-import callCenter from "../../../public/assets/images/callcenter.jpeg";
+import ContactForm from '@/components/Form'
+import { Box, Image as ChakraImage, Flex, Heading } from '@chakra-ui/react'
+import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import callCenter from '../../../public/assets/images/callcenter.jpeg'
 
 const Contact = () => {
-  const t = useTranslations("IndexPage.Aloqa");
-  const imgStyle = {
-    height: "400px",
-    objectFit: "cover",
-    objectPosition: "center",
-  };
+	const t = useTranslations('IndexPage.Aloqa')
+	const imgStyle = {
+		height: '400px',
+		objectFit: 'cover',
+		objectPosition: 'center',
+	}
 
-  return (
-    <Box as="section" mb={"30px"} id="contact">
-      <Heading as={"h2"} size={"lg"} mb={"30px"} id="contact">
-        {t("Sarlavha")}
-      </Heading>
-      <Flex flexDirection={{ base: "column", md: "row" }}>
-        <ChakraImage
-          as={Image}
-          src={callCenter}
-          alt={"callcenter"}
-          width={{ base: "100%", md: "50%" }}
-          style={imgStyle}
-        />
-        <Box
-          width={{ base: "100%", md: "50%" }}
-          bg={"var(--secondary-color)"}
-          display={"flex"}
-          flexDirection={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          px={"20px"}
-          py={"50px"}
-          gap={"30px"}
-        >
-          <Heading size={"md"} mb={"20px"}>
-            {t("FormSarlavha")}
-          </Heading>
-          <ContactForm />
-        </Box>
-      </Flex>
-    </Box>
-  );
-};
+	return (
+		<Box as='section' mb={'30px'} id='contact'>
+			<Heading as={'h2'} size={'lg'} mb={'30px'} id='contact'>
+				{t('Sarlavha')}
+			</Heading>
+			<Flex flexDirection={{ base: 'column', md: 'row' }}>
+				<ChakraImage
+					as={Image}
+					src={callCenter}
+					alt={'callcenter'}
+					width={{ base: '100%', md: '50%' }}
+					style={imgStyle}
+				/>
+				<Box
+					width={{ base: '100%', md: '50%' }}
+					bg={'var(--secondary-color)'}
+					display={'flex'}
+					flexDirection={'column'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					px={'20px'}
+					py={'50px'}
+					gap={'30px'}
+				>
+					<Heading size={'md'} mb={'20px'}>
+						{t('FormSarlavha')}
+					</Heading>
+					<ContactForm />
+				</Box>
+			</Flex>
+		</Box>
+	)
+}
 
-export default Contact;
+export default Contact
